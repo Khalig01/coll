71d78225d8f64933392225bc1971362dca361e0e
Khal01 <aliyev_56@list.ru>
2024-01-14 13:16:06 +0300

    hw

---
+++ src/main/java/homework/coll/CollApplication.java
@@ -0,0 +1,20 @@
+package homework.coll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollApplication.class, args);
+
+
+
+
+
+
+
+	}
+
+}
